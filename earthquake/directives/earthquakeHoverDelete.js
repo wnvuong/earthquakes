4ebc81074114b8776654963074a936bc287e52f6
William Vuong <wvuong@captechconsulting.com>
2015-09-17 15:20:21 -0400

    added delete animation

--- earthquake/directives/earthquakeHoverDelete.js
+++ earthquake/directives/earthquakeHoverDelete.js
@@ -2,26 +2,29 @@ var earthquakeApp = angular.module('earthquakeApp');
 
 earthquakeApp.directive('earthquakeHoverDelete', function($timeout) {
   return {
-    link: function (scope, element, attrs) {
-      $timeout(function() {
-        element.css('pointer-events', 'none');
-        element.css('opacity', 0);
-        $(attrs.earthquakeHoverDelete).hover(function() {
-          element.css('pointer-events', 'auto');
-          element.css('opacity', 1);
-        }, function() {
-
-        });
-      }, 0);
-
-      $timeout(function() {
-        $(attrs.earthquakeHoverDelete + '-wrapper').hover(function() {
-
-        }, function() {
-          element.css('opacity', 0);
+    compile: function(element, attrs) {
+      return {
+        pre: function (scope, element, attrs) {
           element.css('pointer-events', 'none');
-        });
-      });
+          element.css('opacity', 0);
+        },
+        post: function (scope, element, attrs) {
+          $timeout(function() {
+            $(attrs.earthquakeHoverDelete).hover(function() {
+              element.css('pointer-events', 'auto');
+              element.css('opacity', 1);
+            }, function() { });
+          }, 0);
+
+          $timeout(function() {
+            $(attrs.earthquakeHoverDelete + '-wrapper').hover(function() {
+            }, function() {
+              element.css('pointer-events', 'none');
+              element.css('opacity', 0);
+            });
+          });
+        }
+      }
     }
   };
 });
